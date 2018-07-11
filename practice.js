5dd488172283185af9dd640651b13815025af18f
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2018-07-10 18:40:52 -0600

    completed w/ tests passing

--- practice.js
+++ practice.js
@@ -123,26 +123,28 @@ user2.email = 'tyler.mcginnis@devmounta.in'
   Create an empty object called methodCollection.
 */
 
-//Code Here
+var methodCollection = {
 
-
+}
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object. 
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console. 
 */
 
-//Code Here
-
-
+methodCollection.alertHello = function() {
+  alert('hello');
+}
+methodCollection.logHello = function() {
+  console.log('hello');
+}
 
 /*
   Now call your alertHello and logHello methods.
 */
 
-//Code Here
-
-
+methodCollection.alertHello();
+methodCollection.logHello();
 
 ////////// PROBLEM 6 //////////
 
@@ -151,17 +153,21 @@ user2.email = 'tyler.mcginnis@devmounta.in'
   Return a new object with all of the information that you passed in.
 */
 
-//Code Here
-
-
+function makePerson(name, birthday, ssn) {
+  return {name: name, birthday: birthday, ssn: ssn};
+}
 
 ////////// PROBLEM 7 //////////
 
 /*
-  Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object.
+  Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to 
+  make a Credit Card object.
   Return that object so that whenever you invoke makeCard, you get a brand new credit card.
 */
 
-//Code Here
+function makeCard(cardNumber, expirationDate, securityCode) {
+  var newCard = {cardNumber: cardNumber, expirationDate: expirationDate, securityCode: securityCode}
+  return newCard;
+}
 
 
